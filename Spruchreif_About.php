60287a0dd158a6fba5802afe7db8b25685d421f1
Chrisf071 <58389253+Chrisf071@users.noreply.github.com>
2019-12-01 12:45:27 +0100

    Add files via upload

---
+++ Spruchreif_About.php
@@ -0,0 +1,76 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Spruchreif creates a List of Sentences.">
+    <meta name="author" content="Chris Fischer">
+    
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<head></head>     
+<title>Spruchreif</title>
+    
+<body>
+    
+    <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="Spruchreif_Index.php">Spruchreif</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="Spruchreif_Index.php">Home
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="Spruchreif_About.php">About</a>
+            <span class="sr-only">(current)</span>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Spruchreif_Contact.php">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+    <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <ul class="list-unstyled">
+                <li>
+                    <h1 class="mt-5">What is Spruchreif?</h1>
+                    <p class="lead">Spruchreif is a Plattform that saves Sentences that you can create</p>   
+                </li>
+                <li>   
+                    <pre>
+                        <?php
+
+                        ?>
+                    </pre>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </div>
+
+    
+        <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.slim.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+    
+<footer>
+    
+</footer>
+    
+</html>
